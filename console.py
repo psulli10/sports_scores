77c7445d7bd6fd3f9238c033a8df64aea0716713
psulli10 <petersullivan1985@gmail.com>
2021-09-09 21:00:14 +0100

    run_sql helper function added along with repositories directory

---
+++ console.py
@@ -0,0 +1,9 @@
+import pdb
+from models.attribute import Attribute
+from models.player import Player
+
+player_attributes_1 = Attribute(10, 10, 10, 10, 10)
+player_1 = Player("Jamie McGrath", player_attributes_1, 7)
+
+
+pdb.set_trace()
